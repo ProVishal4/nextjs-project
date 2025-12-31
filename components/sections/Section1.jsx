5a9279abebab84c2f8031d4154cf669a21d064f8
ProVishal4 <vishalyaduweb4@gmail.com>
2025-12-31 22:01:43 +0530

    working deployment3

--- components/sections/Section1.jsx
+++ components/sections/Section1.jsx
@@ -1,13 +1,21 @@
-"use client"
-import React from 'react'
-import Navbar from '../header/Navbar';
-//hero section
-const Section1 = () => {
-  return (
-    <div className='border border-red-500 w-full h-20'>
-      <Navbar />;
-    </div>
-  );
-}
+// "use client"
+// import React from 'react'
+// import Navbar from '../header/Navbar';
+// //import Navbar from '@/header/Navbar';
+// //hero section
+// const Section1 = () => {
+//   return (
+//     <div className='border border-red-500 w-full h-20'>
+//       <Navbar />;
+//     </div>
+//   );
+// }
 
-export default Section1
+// export default Section1
+import React from 'react'
+
+export default function Section1() {
+  return (
+    <div>Section1</div>
+  )
+}
